246792ef3676f7d7d4483fac681c884215573fec
Stênio Galvão <steniogalvao@gmail.com>
2018-06-01 16:00:15 -0300

    First commit
    - creating project
    - gitignore

---
+++ src/main/java/br/com/vsgdev/emotionsRoute/emotionsRoute/EmotionsRouteApplication.java
@@ -0,0 +1,12 @@
+package br.com.vsgdev.emotionsRoute.emotionsRoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionsRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionsRouteApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/vsgdev/emotionsRoute/emotionsRoute/EmotionsRouteApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.vsgdev.emotionsRoute.emotionsRoute;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmotionsRouteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
