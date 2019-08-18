ef9011c00f5e26b06d272e37b84ac57f5aee199b
Stenio Galvao <steniogalvao@gmail.com>
2019-08-18 10:52:18 +0300

    TourItem * Removing unsed imports from TourItemRepository *

--- src/main/java/br/com/vsgdev/emotionsRoute/repository/TourItemRepository.java
+++ src/main/java/br/com/vsgdev/emotionsRoute/repository/TourItemRepository.java
@@ -3,7 +3,6 @@ package br.com.vsgdev.emotionsRoute.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.vsgdev.emotionsRoute.model.Person;
 import br.com.vsgdev.emotionsRoute.model.TourItem;
 
 @Repository
